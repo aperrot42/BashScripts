ed2c326093cac3fa494a093141c4bb3f2205d965
Antonin Perrot-Audet <antonin07130@gmail.com>
2010-09-16 09:05:55 -0400

    wortking comparison of LoG and Kishore algo

--- seedKishoreScript.sh
+++ seedKishoreScript.sh
@@ -1,25 +1,71 @@
 #!/bin/bash
-echo "Start procedure for getting foreground out of nuclei and membrane image"
+# $1 is Nuclei raw image
+# $2 is membrane raw image
+# $3 is path to executables (ending with/)
+# $4 is cell radius in um
 
-RADIUS=$3
+
+
+
+RADIUS=$4
+WEIGHTEDRADIUSPLUS=$(echo "$4 * 0.707106781 * 1.55" | bc)
+WEIGHTEDRADIUSMINUS=$(echo "$4 * 0.707106781 * 0.45" | bc)
+RADIUSMAX=$(echo "$4 * 1.35" | bc)
+
+echo "$WEIGHTEDRADIUSPLUS"
+echo "$WEIGHTEDRADIUSMINUS"
+echo "$RADIUSMAX"
 
 #first preprocess data
 #for the nuclei :
 echo "[Preprocess Nuclei]: $1"
-echo `time ./cellPreprocess $1 $1_Preprocess_$RADIUS.mha 0 $RADIUS`
+echo `time $3cellPreprocess $1 $1_Preprocess_$RADIUS.mha 0 $RADIUS`
 #for the membrane :
 echo "[Preprocess Membrane]: $2"
-echo `time ./cellPreprocess $2 $2_Preprocess_$RADIUS.mha 1 $RADIUS`
+echo `time $3cellPreprocess $2 $2_Preprocess_$RADIUS.mha 1 $RADIUS`
+
+
 
 #extract foreground
 echo "[Extract Foreground]"
-echo `time ./cellForeground $1_Preprocess_$RADIUS.mha $2_Peprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha  $1_GaussFit_$RADIUS.mha`
+echo `time $3cellForeground $1_Preprocess_$RADIUS.mha $2_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha  $1_GaussFit_$RADIUS.mha`
 
 #extract features
-echo "[Extract Features]"
-echo `time ./cellFeature $1_Preprocess_$RADIUS.mha $2_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha  $1_Feature_$RADIUS.mha $1_Distance_$RADIUS.mha`
+echo "[Extract Features Kishore]"
+#echo `time ./cellFeature $1_Preprocess_$RADIUS.mha $2_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha  $1_Feature_$RADIUS.mha $1_Distance_$RADIUS.mha`
+echo `time $3gradientWeightedDistanceImageFilter $1_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha $1_FeatureMap_$RADIUS.mha  alpha beta`
+
 
 #extract seeds
-echo "[Extract Seeds]"
-echo `time ./seedExtract $1_Foreground_$RADIUS.mha $1_Feature_$RADIUS.mha $1 $1_Comp_$RADIUS.mha`
+echo "[Extract Seeds Kishore]"
+echo `time $3seedExtract $1_Foreground_$RADIUS.mha $1_FeatureMap_$RADIUS.mha $1 $1_GaussFit_$RADIUS.mha $1_SeedKishore_$RADIUS.txt $1_NucleiImageKishore_$RADIUS.mha`
+
+
+
+
+
+#compute features MLOG
+echo "[Extract Feature MLOG]"
+echo `time $3multiScaleLoGDistanceImageFilter3D $1_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha $1_CompMLOG_$RADIUS.mha $WEIGHTEDRADIUSMINUS $WEIGHTEDRADIUSPLUS 15 2.2 0`
+
+#echo `time $3multiScaleLoGDistanceImageFilter3D $1_Preprocess_$RADIUS.mha $1_Foreground_$RADIUS.mha $1_CompMLOG_$RADIUS.mha 2.2 4.5 15 2.2 0`
+
+#extract maximas (seeds) MLOG
+echo "[Extract MLOG seeds]"
+echo `time $3localMaxExtract $1_CompMLOG_$RADIUS.mha $1_Foreground_$RADIUS.mha $1_SeedMLOG_$RADIUS.txt $1_NucleiImageMLOG_$RADIUS.mha $RADIUSMAX 0.02 2500`
+
+#write image for visual comparison
+echo "[Write image with MLoG seeds]"
+echo `$3blendImageSeeds $1 $1_SeedMLOG_$RADIUS.txt $1_SeedMLOG_$RADIUS.mha`
+
+#write image for visual comparison
+echo "[Write image with Kishore seeds]"
+echo `$3blendImageSeeds $1 $1_SeedKishore_$RADIUS.txt $1_SeedKishore_$RADIUS.mha`
+
+
+#compute statistics for both algorithms
+echo "[Compute statistics]"
+echo `$3validateSeeds $1_Validation.mha $1_SeedKishore_$RADIUS.txt $1_StatKishore_$RADIUS.txt`
+echo `$3validateSeeds $1_Validation.mha $1_SeedMLOG_$RADIUS.txt $1_StatMLOG_$RADIUS.txt`
+
 
